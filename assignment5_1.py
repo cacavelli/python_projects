083d57acfeff58ddc6ebdaff022c97e9b79c971d
cacavelli <53318431+cacavelli@users.noreply.github.com>
2020-03-01 18:11:52 -0300

    Update assignment5_1.py

--- assignment5_1.py
+++ assignment5_1.py
@@ -1,4 +1,6 @@
 """
+Este código foi a resposta de uma atividade do curso de Python da Universidade de Michigan.
+
 Exercise 1: Write a program which repeatedly reads numbers until the user enters "done". 
 Once "done" is entered, print out the total, count, and average of the numbers. 
 If the user enters anything other than a number, detect their mistake using try and except and print an error message and skip to the next number.
