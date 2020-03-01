dd4e11a081f8b02de98b27c5f5e15a837b2a2e2e
cacavelli <53318431+cacavelli@users.noreply.github.com>
2020-03-01 18:06:54 -0300

    Add files via upload

---
+++ assignment5_1.py
@@ -0,0 +1,36 @@
+"""
+Exercise 1: Write a program which repeatedly reads numbers until the user enters "done". 
+Once "done" is entered, print out the total, count, and average of the numbers. 
+If the user enters anything other than a number, detect their mistake using try and except and print an error message and skip to the next number.
+
+
+example:
+Enter a number: 4
+Enter a number: 5
+Enter a number: bad data
+Invalid input
+Enter a number: 7
+Enter a number: done
+16 3 5.333333333333333
+"""
+
+count = 0
+tot = 0.0
+print("Digite 'done' para encerrar\n")
+
+while True:
+    first_data = input("Digite um número: ")
+    if first_data == 'done' :
+        break
+    try:
+        num = float(first_data) 
+    except:
+        print("Entrada inválida")
+        continue #faz com que o loop reinicie e vá para a próxima iteração
+    count = count + 1 #contador
+    tot = tot + num #soma os valores inseridos
+
+print("\nFinito\n")
+print("A soma de todos os números é: ", tot)
+print("Você digitou um total de ", count, "número(s)")
+print("A média dos valores inseridos é: ", tot / count)
---
+++ assignment5_2.py
@@ -0,0 +1,32 @@
+"""5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
+Once 'done' is entered, print out the largest and smallest of the numbers. 
+If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. 
+Enter 7, 2, bob, 10, and 4 and match the output below."""
+
+
+largest = None
+smallest = None
+
+while True:
+    try:
+        num = input("Enter a number: ")
+        if num == "done" : #encerra o programa
+            break
+        number = int(num) #testa para ver se tem erro
+    except ValueError:
+        print("Invalid input") #caso tenha erro, volta para o processo anterior
+        continue
+    
+    if largest is None:
+        largest = number
+    elif number > largest:
+        largest = number
+    
+    if smallest is None:
+        smallest = number
+    elif number < smallest:
+        smallest = number
+    
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
